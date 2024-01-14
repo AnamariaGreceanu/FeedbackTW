322fcdcc962ec4a735e8df28a629c640de8b8f60
CosminOprescu <c.oprescu2002@gmail.com>
2024-01-14 17:20:02 +0200

    PaginaProfSemiFinal

--- front/src/pages/Teacher.jsx
+++ front/src/pages/Teacher.jsx
@@ -1,27 +1,68 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 const TeacherPage = () => {
+  const [activitiesBySubject, setActivitiesBySubject] = useState({});
   const [activities, setActivities] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedActivity, setSelectedActivity] = useState(null);
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [feedbackData, setFeedbackData] = useState(null);
 
-  const buttonStyle = {
-    backgroundColor: '#007bff',
-    color: '#ffffff',
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    marginRight: '10px',
+  const fetchFeedbackData = (title) => {
+    // Simulează o cerere către baza de date pentru feedback
+    // Înlocuiește cu metoda reală de obținere a feedback-ului
+    const simulatedFeedbackData = {
+      date: '2022-03-15',
+      text: 'Feedback-ul pentru activitatea X este pozitiv.',
+    };
+    setFeedbackData(simulatedFeedbackData);
   };
 
   const handleSaveActivity = (activity) => {
-    setActivities([...activities, activity]);
+    const updatedActivities = [...(activitiesBySubject[selectedSubject] || []), activity];
+    setActivitiesBySubject({
+      ...activitiesBySubject, [selectedSubject]: updatedActivities,
+    });
     setIsModalOpen(false); // Închide modalul după salvare
   };
 
-  const handleActivityClick = (activity, history) => {
+  const handleSubjectSelect = (subject) => {
+    setSelectedSubject(subject);
+    setSelectedActivity(null); // Adaugă această linie pentru a reseta activitatea selectată
+    setFeedbackData(null); // Și această linie pentru a reseta feedback-ul
+  };
+
+  useEffect(() => {
+    // Verificăm dacă o materie este selectată și obținem feedback-ul pentru acea materie
+    if (selectedSubject && selectedActivity) {
+      fetchFeedbackData(selectedActivity.title); // Poți înlocui cu metoda reală de obținere a feedback-ului
+    }else{
+      setFeedbackData(null);
+    }
+  }, [selectedActivity]);
+
+  const SubjectsList = () => {
+    const subjects = ['Multimedia', 'TW', 'Android', 'DSAD'];
+
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center' }}>Select Your Subject</h1>
+        <ul>
+          {subjects.map((subject, index) => (
+            <li key={index} style={{ cursor: 'pointer' }} onClick={() => setSelectedSubject(subject)}>
+              {subject}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  if (!selectedSubject) {
+    return <SubjectsList />;
+  }
+
+  const handleActivityClick = (activity) => {
     // Aici deschidem pagina de feedback pentru activitatea selectată
     setSelectedActivity(activity);
   };
@@ -44,7 +85,7 @@ const TeacherPage = () => {
     return (
       <div className="modal" style={{ display: isModalOpen ? 'block' : 'none' }}>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="title">Titlu:</label>
+          <label htmlFor="title">Title:</label>
           <input
             type="text"
             id="title"
@@ -53,7 +94,7 @@ const TeacherPage = () => {
             required
           />
 
-          <label htmlFor="time">Setare Ora:</label>
+          <label htmlFor="time">Set hour:</label>
           <input
             type="time"
             id="time"
@@ -62,7 +103,7 @@ const TeacherPage = () => {
             required
           />
 
-          <label htmlFor="date">Setare Data:</label>
+          <label htmlFor="date">Set data:</label>
           <input
             type="date"
             id="date"
@@ -71,7 +112,7 @@ const TeacherPage = () => {
             required
           />
 
-          <label htmlFor="accessCode">Setare Cod Acces:</label>
+          <label htmlFor="accessCode">Set acces code:</label>
           <input
             type="text"
             id="accessCode"
@@ -80,7 +121,7 @@ const TeacherPage = () => {
             required
           />
 
-          <button type="submit">Salvează</button>
+          <button type="submit">Save</button>
         </form>
         <button onClick={() => setIsModalOpen(false)}>Închide</button>
       </div>
@@ -89,9 +130,25 @@ const TeacherPage = () => {
 
   return (
     <div>
-      <h1 style={{ textAlign: 'center' }}>Pagina Profesorului</h1>
-
-      {activities.map((activity, index) => (
+      <h1 style={{ textAlign: 'center' }}>Teacher's Page</h1>
+      <a
+        style={{
+          position: 'absolute',
+          bottom: '20px',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          backgroundColor: '#007bff',
+          color: '#ffffff',
+          padding: '10px 20px',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer',
+          marginRight: '10px',
+        }}
+        href="#"
+        onClick={() => handleSubjectSelect(null)}>Back to Subjects</a> {/* Apelez functia de revenire la materii  */}
+        <h2 style={{textAlign: 'center'}}>Subject: {selectedSubject}</h2>
+      {(activitiesBySubject[selectedSubject] || []).map((activity, index) => (
         <div
           key={index}
           style={{
@@ -101,8 +158,7 @@ const TeacherPage = () => {
             marginTop: '10px',
             cursor: 'pointer',
           }}
-          onClick={() => handleActivityClick(activity)}
-        >
+          onClick={() => handleActivityClick(activity)}>
           <p>Titlu: {activity.title}</p>
           <p>Ora: {activity.time}</p>
           <p>Data: {activity.date}</p>
@@ -112,36 +168,28 @@ const TeacherPage = () => {
 
       {isModalOpen ? null : (
         <button
-          style={buttonStyle}
-          onClick={() => setIsModalOpen(true)}
-        >
-          Adaugă Activitate
-        </button>
+          style={{
+            backgroundColor: '#007bff',
+            color: '#ffffff',
+            padding: '10px 20px',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            marginRight: '10px',
+          }}
+          onClick={() => setIsModalOpen(true)}>Add Activity</button>
       )}
 
       {isModalOpen && <AddActivityModal />}
 
       {selectedActivity && (
         <div>
-          <h2>Feedback pentru activitatea: {selectedActivity.title}</h2>
-          {/* Afișează feedback-ul aici, în funcție de activitatea selectată */}
-          <button onClick={closeFeedback}>Închide</button>
+          <h2>Feedback for activity: {selectedActivity.title}</h2>
+          <p>Feedback date: {feedbackData ? feedbackData.date : 'Loading ...'}</p>
+          <p>Feedback: {feedbackData ? feedbackData.text: 'Loading...'}</p>
         </div>
       )}
     </div>
   );
 };
-
-const FeedbackPage = ({ match }) => {
-  // Aici vei adăuga logica pentru a prelua feedback-ul din baza de date
-  const feedbackData = { /* ... */ };
-
-  return (
-    <div>
-      <h2>Feedback pentru activitatea: {match.params.title}</h2>
-      {/* Afișează feedback-ul în funcție de datele obținute din baza de date */}
-    </div>
-  );
-};
-
 export default TeacherPage;
